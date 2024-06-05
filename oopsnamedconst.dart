2137227c5c6e5c33794d48d2cae3c86fe08db203
Murtazakatyar <168894683+Murtazakatyar@users.noreply.github.com>
2024-06-05 11:12:22 +0500

    Add files via upload
    dart Oops named Constructor

---
+++ oopsnamedconst.dart
@@ -0,0 +1,20 @@
+void main() {
+  var obj2 = Student.namedConst();
+}
+
+class Student {
+
+  Student.namedConst() {
+    
+    String name = 'Asad';
+    int age = 20;
+    int roolNo = 101;
+    int Sclass = 8;
+    print('Student Name is  : $name');
+    print('Student Age is  : $age');
+    print('Student Rool No is  : $roolNo');
+    print('Student Class is  : $Sclass');
+
+    
+  }
+}
